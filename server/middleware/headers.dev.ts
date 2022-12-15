7d55fadc843371bdd68db1b7b6ee5423da4ed935
zernonia <zernonia@gmail.com>
2022-12-16 02:00:18 +0800

    remove dev only coep

--- server/middleware/headers.dev.ts
+++ server/middleware/headers.dev.ts
@@ -1,6 +1,4 @@
 export default defineEventHandler((event) => {
-  if (process.env.NODE_ENV === "development") {
-    event.node.res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    event.node.res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  }
+  event.node.res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  event.node.res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
 });
