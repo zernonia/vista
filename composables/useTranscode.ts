bbf8459202c1bc546be65da1240ebaeb5b385b7c
zernonia <zernonia@gmail.com>
2022-12-19 13:49:47 +0800

    fix error message

--- composables/useTranscode.ts
+++ composables/useTranscode.ts
@@ -92,7 +92,7 @@ export const useTranscode = createSharedComposable(() => {
     } catch (err) {
       console.log(err);
       message.value = "Something wrong. Please contact Zernonia";
-      throw Error(err);
+      throw Error("Something wrong. Please contact Zernonia");
     }
   };
 
